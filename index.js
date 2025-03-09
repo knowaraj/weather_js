440f812849266f7830a0fe8774c42d15d1843402
knowaraj <sendmailtoaraj@gmail.com>
2025-03-09 21:39:33 +0545

    Variables declarations

---
+++ index.js
@@ -0,0 +1,5 @@
+const action = document.querySelector("cityInput");
+const city = document.querySelector(".city");
+const maindiv = document.querySelector(".maindiv");
+const apiKey = "cd9b84fc506f8603724b1c9b71bf23a0";
+
