14476bd95d7ac6504f092bcab5c944f8863be955
knowaraj <sendmailtoaraj@gmail.com>
2025-03-09 21:49:47 +0545

    Error message

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const action = document.querySelector("cityInput");
+const action = document.querySelector(".cityInput");
 const city = document.querySelector(".city");
 const maindiv = document.querySelector(".maindiv");
 const apiKey = "cd9b84fc506f8603724b1c9b71bf23a0";
 
+action.addEventListener("submit",event =>{
+    event.preventDefault();
+    const cityname = city.value;
+    if(cityname){
+
+    }
+    else
+    displayError("Please Enter a City");
+})
+async function weatherDAta(cityName) {
+    
+}
+function displayWeather(data){
+
+}
+function displayError(message){
+    const errorMsg = document.createElement("p");
+    errorMsg.textContent = message;
+    errorMsg.classList.add("error");
+
+    maindiv.textContent = "";
+    maindiv.style.display = "flex";
+    maindiv.appendChild(errorMsg);
+}
